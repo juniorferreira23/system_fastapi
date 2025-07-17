6d6fe51684e90dedd14b4f61a928b13a7aa34508
juniorferreira23 <junior.ferreira@outlook.com>
2025-07-17 13:05:14 -0300

    :sparkles: feat: ensuring that the event loop works correctly on Windows

--- src/app.py
+++ src/app.py
@@ -1,8 +1,13 @@
+import asyncio
+import sys
 from http import HTTPStatus
 
 from fastapi import FastAPI
 
 from src.routers import auth, todos, users
+
+if sys.platform == 'win32':  # pragma: no cover
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 app = FastAPI()
 
