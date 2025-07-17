9b71b317778c4c025459077acae89f2188a8518c
juniorferreira23 <junior.ferreira@outlook.com>
2025-07-17 19:24:04 -0300

    :test_tube: test: configured test container

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,7 @@ import pytest_asyncio
 from fastapi.testclient import TestClient
 from sqlalchemy import event
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from testcontainers.postgres import PostgresContainer
 
 from src.app import app
 from src.database import get_session
@@ -27,10 +28,15 @@ def client(session):
     app.dependency_overrides.clear()
 
 
-@pytest_asyncio.fixture
-async def session():
-    engine = create_async_engine(Settings().DATABASE_URL)  # type: ignore
+@pytest.fixture(scope='session')
+def engine():
+    with PostgresContainer('postgres:latest', driver='psycopg') as postgres:
+        _engine = create_async_engine(postgres.get_connection_url())
+        yield _engine
 
+
+@pytest_asyncio.fixture
+async def session(engine):
     async with engine.begin() as conn:
         await conn.run_sync(table_registry.metadata.create_all)
 
